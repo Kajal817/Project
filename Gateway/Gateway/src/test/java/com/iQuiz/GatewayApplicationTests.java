3974d99f4f389949fff0f6108d3bc29d28a2bc5e
Kajal817 <sharmakajal86936@gmail.com>
2025-05-01 13:51:00 +0530

    Changing test folder names

--- Gateway/Gateway/src/test/java/com/iQuiz/GatewayApplicationTests.java
+++ Gateway/Gateway/src/test/java/com/iQuiz/GatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.art;
+package com.iQuiz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RegistryApplicationTests {
+class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
