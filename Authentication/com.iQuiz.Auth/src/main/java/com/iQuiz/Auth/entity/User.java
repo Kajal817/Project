dcdaa5f0de155029f689f5cfc9be2da7a491d337
Kajal817 <sharmakajal86936@gmail.com>
2025-05-04 15:20:09 +0530

    Auth Microservice

--- Authentication/com.iQuiz.Auth/src/main/java/com/iQuiz/Auth/entity/User.java
+++ Authentication/com.iQuiz.Auth/src/main/java/com/iQuiz/Auth/entity/User.java
@@ -1,5 +1,40 @@
 package com.iQuiz.Auth.entity;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name= "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	@Pattern(regexp = "^[a-zA-Z]+$", message = "Full name contains only alphabetic character and whitespace")
+	@Size(min=3, message="Full name must contain atleast threes characters")
+	private String fullName;
+	
+	@Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,7}$", message = "Email is invalid. Please try again")
+	private String email;
+	
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "The password must be atleast 8 character long. Password should contains alteast one upper case alphabet, one lower case alphabet, one digit and one special character")
+	private String password;
+	
+	private String role;
 }
