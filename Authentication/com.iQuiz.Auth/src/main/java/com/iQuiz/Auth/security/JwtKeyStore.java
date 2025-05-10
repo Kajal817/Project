9d79548c30cf2f7558bbe2d770603c983563374f
Kajal817 <sharmakajal86936@gmail.com>
2025-05-10 13:33:15 +0530

    JwtKeyStore class completed

--- Authentication/com.iQuiz.Auth/src/main/java/com/iQuiz/Auth/security/JwtKeyStore.java
+++ Authentication/com.iQuiz.Auth/src/main/java/com/iQuiz/Auth/security/JwtKeyStore.java
@@ -1,5 +1,46 @@
 package com.iQuiz.Auth.security;
 
-public class JwtKeyStore {
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtKeyStore {
+	private static final Logger logger = LoggerFactory.getLogger(JwtKeyStore.class);
+	private final Map<String, String> keys = new HashMap<>();
+	private final String currentKeyId;
+	
+	public JwtKeyStore(@Value("${jwt.keys.current}") String currentKeyId, @Value("#{${jwt.keys}}") Map<String, String> keys) {
+		this.currentKeyId = currentKeyId;
+		this.keys.putAll(keys);
+		logger.info("JWT Key Store intialized. Current KeyId: {}", currentKeyId);
+	}
+	
+	
+	public Key getKeyById(String keyId) {
+		logger.debug("Fetching Key for key id: {}", keyId);
+		String secret = keys.get(keyId);
+		if(secret == null) {
+			logger.error("Key Id {} not found in configuration.", keyId);
+			throw new IllegalArgumentException("Invalid Key Id: "+ keyId);
+		}
+		return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+	}
+	
+	public Key getCurrentKey() {
+		logger.debug("Fetching current Signing Key (Key Id: {})", currentKeyId);
+		return getKeyById(currentKeyId);
+	}
+	
+	public String getCurrentKeyId() {
+		return currentKeyId;
+	}
 }
--- Authentication/com.iQuiz.Auth/src/main/java/com/iQuiz/Auth/security/JwtTokenProvider.java
+++ Authentication/com.iQuiz.Auth/src/main/java/com/iQuiz/Auth/security/JwtTokenProvider.java
@@ -3,6 +3,8 @@ package com.iQuiz.Auth.security;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,9 @@ import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtTokenProvider {
+	
+	private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
+	
 	@Value("${app.jwt.secret}")
     private String jwtSecret;
 	
