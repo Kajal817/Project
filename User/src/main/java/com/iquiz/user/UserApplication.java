cbf4c0a22d8eda4ed4104229a61dd11b9214ef4d
Kajal817 <sharmakajal86936@gmail.com>
2025-05-13 22:01:54 +0530

    UserApplication

--- User/src/main/java/com/iquiz/user/UserApplication.java
+++ User/src/main/java/com/iquiz/user/UserApplication.java
@@ -1,13 +1,22 @@
 package com.iquiz.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class UserApplication {
+	
+	public static final Logger logger = LoggerFactory.getLogger(UserApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
+		logger.info("UserApplication is up and running.");
 	}
 
 }
